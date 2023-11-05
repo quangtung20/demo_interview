82959164a9ceaeda055d4ba96d75ef56b7075168
quangtung20 <tranquangtung20111999@gmail.com>
2023-11-05 21:51:39 +0700

    add s3 backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3statebackend051123tung"
+    dynamodb_table = "state-lock"
+    key            = "global/mystatefile/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 ## requester
 provider "aws" {
   alias  = "requester"
